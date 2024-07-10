6b885dabde5c07c6c2bbc318135947f1c6787352
Navin Kumar <vrushankrao1@gmail.com>
2024-07-10 15:08:27 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chatappvru.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
